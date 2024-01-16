3817a9afd81a6aaea6a92508efc3d696c07845f0
andreasvarga <varga.andreas@gmail.com>
2024-01-16 12:24:19 +0100

    Check test condition

--- test/test_iterative.jl
+++ test/test_iterative.jl
@@ -26,6 +26,7 @@ using IterativeSolvers
 
         # If you start from the exact solution, you should converge immediately
         x, info = cgls(A, b; x0 = A \ b, abstol=n*n*eps(real(T)), reltol)
+        @show info
         @test info.flag == 1 && info.iter <= 2
 
         # All-zeros rhs should give all-zeros lhs
